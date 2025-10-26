f0b004873a865fffbebe181b133fc7971de3051d
Jordi Orriols <info@jordiorriols.cat>
2025-10-26 18:23:43 +0100

    chore: make thunderstorm darker

--- src/components/weather/thunderstorm.tsx
+++ src/components/weather/thunderstorm.tsx
@@ -20,31 +20,17 @@ export default function ThunderstormScene({ timeOfDay }) {
   }, []);
 
   return (
-    <div className="absolute inset-0 bg-gradient-to-b from-[#1F1B24] via-[#2D2832] to-[#4B3F72]">
+    <div className="absolute inset-0 bg-gradient-to-b from-[#2C2B2F] to-[#3A444F]">
       {/* Lightning Flash */}
       {lightning && (
         <div className="absolute inset-0 bg-white opacity-40 animate-flash z-10"></div>
       )}
 
       {/* Clouds */}
-      <Clouds max={50} color="black" />
-
-      {/* Lightning Bolt */}
-      {lightning && (
-        <svg
-          className="absolute top-20 left-1/2 transform -translate-x-1/2 w-16 h-64 z-20"
-          viewBox="0 0 64 256"
-        >
-          <path
-            d="M32,0 L20,100 L40,100 L28,256 L52,100 L36,100 Z"
-            fill="#FFD700"
-            className="drop-shadow-2xl"
-          />
-        </svg>
-      )}
+      <Clouds max={50} />
 
       {/* Rain */}
-      <Rain max={50} />
+      <Rain max={500} />
 
       {/* City Skyline */}
       <CitySkyline fill="#0F0D12" />
