89b97c760c1ceb84dc2e64049159b4b16bc0325f
Jordi Orriols <info@jordiorriols.cat>
2025-10-26 21:18:03 +0100

    chore: using config objects

--- src/components/weather/raining.tsx
+++ src/components/weather/raining.tsx
@@ -4,20 +4,42 @@ import CitySkyline from "./elements/skyline";
 import Clouds from "./elements/cloud";
 import Rain from "./elements/rain";
 import Stars from "./elements/stars";
+import { TimeOfDayType } from "@/pages/portfolio";
+
+interface configType {
+  bgFrom: string;
+  bgVia: string;
+  bgTo: string;
+}
+
+const config: Record<TimeOfDayType, configType> = {
+  morning: {
+    bgFrom: "#6B7280",
+    bgVia: "#8B92A0",
+    bgTo: "#9CA3AF",
+  },
+  day: {
+    bgFrom: "#6B7280",
+    bgVia: "#8B92A0",
+    bgTo: "#9CA3AF",
+  },
+  afternoon: {
+    bgFrom: "#64748B",
+    bgVia: "#7B8799",
+    bgTo: "#94A3B8",
+  },
+  night: {
+    bgFrom: "#374151",
+    bgVia: "#4B5563",
+    bgTo: "#6B7280",
+  },
+};
 
 export default function RainyScene({ timeOfDay }) {
-  const getGradient = () => {
-    if (timeOfDay === "morning") {
-      return "from-[#6B7280] via-[#8B92A0] to-[#9CA3AF]";
-    } else if (timeOfDay === "afternoon") {
-      return "from-[#64748B] via-[#7B8799] to-[#94A3B8]";
-    } else {
-      return "from-[#374151] via-[#4B5563] to-[#6B7280]";
-    }
-  };
-
   return (
-    <div className={`absolute inset-0 bg-gradient-to-b ${getGradient()}`}>
+    <div
+      className={`absolute inset-0 bg-gradient-to-b from-[${config[timeOfDay].bgFrom}] via-[${config[timeOfDay].bgVia}] to-[${config[timeOfDay].bgTo}]`}
+    >
       {/* Stars */}
       {timeOfDay === "night" ? <Stars max={150} /> : null}
       {/* Clouds */}
