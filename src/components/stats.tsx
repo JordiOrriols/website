52145196c4862dd374b08e075b13fdfc0ef3cc81
Jordi Orriols <info@jordiorriols.cat>
2025-10-28 00:09:48 +0100

    fix: color for stats

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -18,7 +18,7 @@ export default function Stats(props: Props) {
           onClick={option.onClick}
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 400, damping: 17 }}
-          className="text-center cursor-pointer hover:bg-gray-50 rounded-xl p-4 transition-colors duration-200"
+          className="text-center cursor-pointer hover:bg-gray-100 rounded-xl p-4 transition-colors duration-200"
         >
           <div className="text-center">
             <div className="text-5xl font-extralight text-gray-800 mb-2">
