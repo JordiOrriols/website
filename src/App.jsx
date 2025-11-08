e40c1f22221baf914362acd1e108ad9e8643ea90
Jordi Orriols <info@jordiorriols.cat>
2025-11-08 22:04:17 +0100

    chore: add error boundary on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Suspense } from "react";
 import Portfolio from "./pages/portfolio";
 import "./lib/i18n";
+import { ErrorBoundary } from "react-error-boundary";
 
 export default () => (
   <Suspense fallback="loading">
-    <Portfolio />
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Portfolio />
+    </ErrorBoundary>
   </Suspense>
 );
