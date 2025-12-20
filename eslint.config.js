2a78afe1386aad0fef217b0f8660d9099d498674
Jordi Orriols <info@jordiorriols.cat>
2025-12-20 15:46:24 +0100

    chore: configure eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import js from "@eslint/js";
+import typescript from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      ".git",
+      ".next",
+      "out",
+      "coverage",
+      "*.log",
+      ".DS_Store",
+      ".env*",
+    ],
+  },
+  js.configs.recommended,
+  ...typescript.configs.recommended,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        browser: true,
+        es2021: true,
+        node: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  prettier,
+];
